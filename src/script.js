d0abba10ac465420c0001f9e563376e637354d0e
kasianiemiec <kasia.a.niemiec@gmail.com>
2025-03-12 18:02:19 +0200

    fixed the sydney timezone mistake

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ function updateTime() {
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Pacific/Sydney");
+  let sydneyTime = moment().tz("Australia/Sydney");
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
   sydneyTimeElement.innerHTML = sydneyTime.format(
     "h:mm:ss [<small>]A[</small>]"
